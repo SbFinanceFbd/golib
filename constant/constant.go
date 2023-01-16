a7fc2d21de0b96f1e1bb04966e8edf50421d1e58
shivakumar <shivukumar076@gmail.com>
2023-01-16 18:21:32 +0530

    constant added

--- constant/constant.go
+++ constant/constant.go
@@ -3,4 +3,30 @@ package constant
 const (
 	YES = "YES"
 	NO  = "NO"
+
+	// Loan status
+	LOAN_REGULAR     = "REGULAR"
+	LOAN_GRACEPERIOD = "GRACEPERIOD"
+	LOAN_IRREGULAR   = "IRREGULAR"
+	LOAN_INACTIVE    = "INACTIVE"
+	LOAN_CLOSED      = "CLOSED"
+
+	// RD status
+	RD_ACTIVE   = "ACTIVE"
+	RD_INACTIVE = "INACTIVE"
+	RD_HOLD     = "HOLD"
+	RD_CLOSED   = "CLOSED"
+
+	// FD status
+	FD_ACTIVE   = "ACTIVE"
+	FD_INACTIVE = "INACTIVE"
+	FD_HOLD     = "HOLD"
+	FD_CLOSED   = "CLOSED"
+
+	// Pigmy status
+	PIGMY_ACTIVE      = "ACTIVE"
+	PIGMY_INACTIVE    = "INACTIVE"
+	PIGMY_HAS_LOAN    = "HAS_LOAN"
+	PIGMY_LOAN_CLOSED = "LOAN_CLOSED"
+	PIGMY_CLOSED      = "CLOSED"
 )
