a1c28b18575982c9a2d30dee06a629465636c824
shivakumar <shivukumar076@gmail.com>
2023-01-22 17:53:45 +0530

    time added

---
+++ utils/DateAndTimeUtils.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+func GetYears(difference time.Duration) int64 {
+	return int64(difference.Hours() / 24 / 365)
+}
+
+func GetMonths(difference time.Duration) int64 {
+	return int64(difference.Hours() / 24 / 30)
+}
+
+func GetWeeks(difference time.Duration) int64 {
+	return int64(difference.Hours() / 24 / 7)
+}
+
+func GetDays(difference time.Duration) int64 {
+	return int64(difference.Hours() / 24)
+}
+
+func GetHours(difference time.Duration) (int64, error) {
+	return strconv.ParseInt(fmt.Sprintf("%.f", difference.Hours()), 10, 64)
+}
+
+func GetMinutes(difference time.Duration) (int64, error) {
+	return strconv.ParseInt(fmt.Sprintf("%.f", difference.Minutes()), 10, 64)
+}
+
+func GetSeconds(difference time.Duration) (int64, error) {
+	return strconv.ParseInt(fmt.Sprintf("%.f", difference.Seconds()), 10, 64)
+}
