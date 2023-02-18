17598ab8362e5ce2152702020ff7f8b1fe6c823f
shivakumar <shivukumar076@gmail.com>
2023-02-18 23:04:49 +0530

    Initial found

--- constant/txnCategory/txnCategory.go
+++ constant/txnCategory/txnCategory.go
@@ -1,27 +1,29 @@
 package txnCategory
 
 const (
-	IF  = "IF"  // Initial found
-	LS  = "LS"  // Loan Sanction
-	LSM = "LSM" // Loan Sanction Mistake
-	LI  = "LI"  // Loan Interest
-	LC  = "LC"  // Loan Collection
-	LCM = "LCM" // Loan Collection Mistake
-	LPC = "LPC" // Loan Penalty Collection
-	RC  = "RC"  // RD Collection
-	RCM = "RCM" // RD Collection Mistake
-	RM  = "RM"  // RD Maturity
-	RMM = "RMM" // RD Maturity Mistake
-	RI  = "RI"  // RD Interest
-	FC  = "FC"  // FD Collection
-	FCM = "FCM" // FD Collection Mistake
-	FI  = "FI"  // FD Interest
-	FM  = "FM"  // FD Maturity
-	FMM = "FMM" // FD Maturity Mistake
-	PC  = "PC"  // Pigmy Collection
-	PCM = "PCM" // Pigmy Collection Mistake
-	PI  = "PI"  // Pigmy Interest
-	PM  = "PM"  // Pigmy Maturity
-	PMM = "PMM" // Pigmy Maturity Mistake
-	BT  = "BT"  // Branch Transfer
+	INF   = "INF"   // Initial found
+	IVF   = "IVF"   // Invest found
+	LS    = "LS"    // Loan Sanction
+	LSM   = "LSM"   // Loan Sanction Mistake
+	LI    = "LI"    // Loan Interest
+	LC    = "LC"    // Loan Collection
+	LCM   = "LCM"   // Loan Collection Mistake
+	LPC   = "LPC"   // Loan Penalty Collection
+	RC    = "RC"    // RD Collection
+	RCM   = "RCM"   // RD Collection Mistake
+	RM    = "RM"    // RD Maturity
+	RMM   = "RMM"   // RD Maturity Mistake
+	RI    = "RI"    // RD Interest
+	FC    = "FC"    // FD Collection
+	FCM   = "FCM"   // FD Collection Mistake
+	FI    = "FI"    // FD Interest
+	FM    = "FM"    // FD Maturity
+	FMM   = "FMM"   // FD Maturity Mistake
+	PC    = "PC"    // Pigmy Collection
+	PCM   = "PCM"   // Pigmy Collection Mistake
+	PI    = "PI"    // Pigmy Interest
+	PM    = "PM"    // Pigmy Maturity
+	PMM   = "PMM"   // Pigmy Maturity Mistake
+	BT    = "BT"    // Branch Transfer
+	OTHER = "OTHER" // OTHER Expenditure
 )
